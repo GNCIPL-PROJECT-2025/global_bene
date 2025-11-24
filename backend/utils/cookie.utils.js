4b2ba874132187e900043e6db09b24be3bf7edb1
Vivek Dubey <vivek.dubey0305@gmail.com>
2025-11-24 18:49:33 +0530

    Update cookie.utils.js

--- backend/utils/cookie.utils.js
+++ backend/utils/cookie.utils.js
@@ -1,11 +1,39 @@
+// const cookieToken = async (user, res) => {
+
+//     const accessToken = await user.generateAccessToken()
+//     const refreshToken = await user.generateRefreshToken()
+
+//     user.refreshToken = refreshToken;
+
+//     return res.status(201)
+//         .json({
+//             success: true,
+//             message: `User created`,
+//             user, accessToken, refreshToken
+//         })
+// }
+
+// export { cookieToken }
+
+
 const cookieToken = async (user, res) => {
 
     const accessToken = await user.generateAccessToken()
     const refreshToken = await user.generateRefreshToken()
 
     user.refreshToken = refreshToken;
+    const isProduction = process.env.NODE_ENV === "production";
+
+    const options = {
+        httpOnly: true,
+        secure: true,
+        maxAge: 7 * 24 * 60 * 60 * 1000,  // 7 days in milliseconds
+        sameSite: isProduction ? "none" : "lax" // "none" needed for Vercel<->Render
+    }
 
     return res.status(201)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", refreshToken, options)
         .json({
             success: true,
             message: `User created`,
