a29feae118c458da93150e71aeb925d819275288
Vivek Dubey <vivek.dubey0305@gmail.com>
2025-11-25 10:10:48 +0530

    Change email transport to secure mode

--- backend/utils/mail.utils.js
+++ backend/utils/mail.utils.js
@@ -3,7 +3,7 @@ export const sendEmail = async ({ email, subject, message }) => {
 
     const transporter = nodeMailer.createTransport({
         service: process.env.SMTP_SERVICE,
-        secure: false, // true for 465, false for other ports
+        secure: true, // true for 465, false for other ports
         auth: {
             user: process.env.SMTP_MAIL,
             pass: process.env.SMTP_PASSWORD,
